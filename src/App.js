66ca0575831d52f7b9b51cfb2e08d41e3ec3f8d8
Kris Urbas <krzysu@interia.pl>
2020-05-31 16:52:09 +0200

    Add gnosis dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Button, Title, Text, Loader } from "@gnosis.pm/safe-react-components";
+import { theme } from "@gnosis.pm/safe-react-components";
 
-function App() {
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Title size="md">Idle finance</Title>
+        <Button size="lg" color="primary" variant="contained">
+          Deposit
+        </Button>
+        <Loader />
+        <Text size="lg">Test text</Text>
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
